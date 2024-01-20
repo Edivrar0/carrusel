70d69c12aa5bbc8763e9d81dd3a54e930a869dd9
Edivrar0 <edgarramirez74@aragon.unam.mx>
2024-01-20 15:06:49 -0600

    Se utilizó html, css, js

---
+++ app.js
@@ -0,0 +1,70 @@
+let imagenes = [
+    {
+        "url": "img/img1.webp",
+        "nombre": "Ciudad de México",
+        "descripcion": "Chilaquiles, elotes, tamales, quesadillas, tacos"
+
+    },
+    {
+        "url": "img/img2.webp",
+        "nombre": "Estados Unidos",
+        "descripcion": "Hamburguesa, hot dog, macarrones con queso, pizza estilo chicago"
+
+    },
+    {
+        "url": "img/img3.webp",
+        "nombre": "Japón",
+        "descripcion": "Sushi, sashimi, ramen, temaki, udon"
+
+    },
+]
+
+
+let atras = document.getElementById('atras');
+let adelante = document.getElementById('adelante');
+let imagen = document.getElementById('img');
+let puntos = document.getElementById('puntos');
+let texto = document.getElementById('texto')
+let actual = 0
+posicionCarrusel()
+
+atras.addEventListener('click', function(){
+    actual -=1
+
+    if (actual == -1){
+        actual = imagenes.length - 1
+    }
+
+    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+    texto.innerHTML = `
+    <h3>${imagenes[actual].nombre}</h3>
+    <p>${imagenes[actual].descripcion}</p>
+    `
+    posicionCarrusel()
+})  
+adelante.addEventListener('click', function(){
+    actual +=1
+
+    if (actual == imagenes.length){
+        actual = 0
+    }
+
+    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+    texto.innerHTML = `
+    <h3>${imagenes[actual].nombre}</h3>
+    <p>${imagenes[actual].descripcion}</p>
+    `
+    posicionCarrusel()
+})  
+
+function posicionCarrusel() {
+    puntos.innerHTML = ""
+    for (var i = 0; i <imagenes.length; i++){
+        if(i == actual){
+            puntos.innerHTML += '<p class="bold">.<p>'
+        }
+        else{
+            puntos.innerHTML += '<p>.<p>'
+        }
+    } 
+}
